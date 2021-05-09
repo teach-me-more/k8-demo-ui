a000f45c803699f0bd70161d3053dc19a06d0702
chaudharyamardeep <chaudharyamardeep@gmail.com>
2021-05-09 16:33:16 -0500

    Added alternative script

--- scripts/start-app.sh
+++ scripts/start-app.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Starting my application"
-cd /home/ec2-user/
+cd /home/ec2-user
 echo "In my app directory"
-sudo java -jar spring-hello.jar
+sudo /usr/bin/java -jar -Dserver.port=8080 \
+    *.jar > /dev/null 2> /dev/null < /dev/null &
 echo "Application is started"
+ 
